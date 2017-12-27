e521d4315dc9efa24229641cae9d4f2cb8338f24
ᜋᜃ᜔ᜐ᜔ ᜉᜒᜇᜒᜈ᜔  ᜵ Maks Perin <perinmaxangelo@yahoo.com>
2017-12-27 13:22:11 +0800

    Add files via upload

---
+++ PH-SENSOR-122717/PH-SENSOR-122717.ino
@@ -0,0 +1,114 @@
+/*
+ # This sample code is used to test the pH meter V1.0.
+ # Editor : YouYou
+ # Ver    : 1.0
+ # Product: analog pH meter
+ # SKU    : SEN0161
+*/
+#define SensorPin A1            //pH meter Analog output to Arduino Analog Input 0
+#define Offset 0.00           //deviation compensate
+#define LED 13
+#define samplingInterval 20
+#define printInterval 800
+#define ArrayLenth  40    //times of collection
+int pHArray[ArrayLenth];   //Store the average value of the sensor feedback
+int pHArrayIndex=0;
+const int solepin = 22;
+const int solepin1 = 28;    
+void setup(void)
+{
+  pinMode(LED,OUTPUT);  
+  pinMode(2,OUTPUT); 
+
+   pinMode(solepin,OUTPUT);
+   pinMode(solepin1,OUTPUT);
+ 
+  Serial.begin(9600);  
+ // Serial.println("pH meter experiment!");    //Test the serial monitor
+}
+void loop()
+{
+  static unsigned long samplingTime = millis();
+  static unsigned long printTime = millis();
+  static float pHValue,voltage;
+  float pHTrue;  
+  if(millis()-samplingTime > samplingInterval)
+  {
+      pHArray[pHArrayIndex++]=analogRead(SensorPin);
+      if(pHArrayIndex==ArrayLenth)pHArrayIndex=0;
+      voltage = avergearray(pHArray, ArrayLenth)*5.0/1024;
+      pHValue = 3.5*voltage+Offset;
+      samplingTime=millis();
+  }
+  if(millis() - printTime > printInterval)   //Every 800 milliseconds, print a numerical, convert the state of the LED indicator
+  {
+//  Serial.print("Voltage:|");
+   //     Serial.print(voltage,2);
+     //   Serial.print("    pH value: |");
+        pHTrue = pHValue;
+        Serial.print("|");
+        Serial.print(pHTrue);
+        Serial.println(" pH");
+        Serial.print("|");
+        Serial.print(pHTrue+7);
+        Serial.println(" pH");
+
+        digitalWrite(LED,digitalRead(LED)^1);
+        printTime=millis();
+  }
+
+ if (pHValue < 7){
+//  digitalWrite(2, HIGH);
+  digitalWrite(solepin,LOW);
+  digitalWrite(solepin1,LOW);
+ // Serial.println("                ACIDIC");
+  
+  }
+
+  else{
+  //  digitalWrite(2, LOW);
+    digitalWrite(solepin,HIGH);
+    digitalWrite(solepin1,HIGH);
+    }
+}
+
+
+double avergearray(int* arr, int number){
+  int i;
+  int max,min;
+  double avg;
+  long amount=0;
+  if(number<=0){
+//   Serial.println("Error number for the array to avraging!/n");
+    return 0;
+  }
+  if(number<5){   //less than 5, calculated directly statistics
+    for(i=0;i<number;i++){
+      amount+=arr[i];
+    }
+    avg = amount/number;
+    return avg;
+  }else{
+    if(arr[0]<arr[1]){
+      min = arr[0];max=arr[1];
+    }
+    else{
+      min=arr[1];max=arr[0];
+    }
+    for(i=2;i<number;i++){
+      if(arr[i]<min){
+        amount+=min;        //arr<min
+        min=arr[i];
+      }else {
+        if(arr[i]>max){
+          amount+=max;    //arr>max
+          max=arr[i];
+        }else{
+          amount+=arr[i]; //min<=arr<=max
+        }
+      }//if
+    }//for
+    avg = (double)amount/(number-2);
+  }//if
+  return avg;
+}
